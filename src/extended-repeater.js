9be9642110c6f8a7b54fae4580dce56c7479fd7c
aehmeve <ehmorizehere@gmail.com>
2023-12-20 00:28:18 +0900

    feat: solve extended repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,57 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  str = `${str}`;
+
+  if (options.separator === undefined) {
+    options.separator = '+';
+  }
+  if (options.repeatTimes === undefined) {
+    options.repeatTimes = 1;
+  }
+  if (options.addition !== undefined) {
+    options.addition = `${options.addition}`;
+  }
+  if (typeof options.addition === 'string' && options.additionRepeatTimes === undefined) {
+    options.additionRepeatTimes = 1;
+  }
+  if (options.addition && options.additionRepeatTimes > 1 && options.additionSeparator === undefined) {
+    options.additionSeparator = '|';
+  } else if (options.addition && options.additionRepeatTimes > 1) {
+    options.additionSeparator = options.additionSeparator.toString();
+  }
+
+  let mainString = '';
+  let additionString = '';
+  let repeatedLine = '';
+
+  if (options.addition) {
+    if (typeof options.additionSeparator === 'string' && options.additionRepeatTimes > 1) {
+      additionString = `${options.addition}${options.additionSeparator}`.repeat(options.additionRepeatTimes);
+      additionString = additionString.slice(0, additionString.length - options.additionSeparator.length);
+    } else {
+      additionString = options.addition;
+    }
+  }
+
+  if (options.repeatTimes > 1) {
+    mainString = `${str}${additionString}${options.separator}`;
+    mainString = mainString.slice(0, mainString.length - options.separator.length);
+  } else if (options.repeatTimes === 1 && additionString) {
+    mainString = `${str}${additionString}`;
+  } else {
+    mainString = `${str}`;
+  }
+
+  for (let i = 0; i < options.repeatTimes; i += 1) {
+    repeatedLine += mainString
+    if (i < options.repeatTimes - 1) {
+      repeatedLine += `${options.separator}`;
+    }
+  }
+
+  return repeatedLine;
 }
 
 module.exports = {
